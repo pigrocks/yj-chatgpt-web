1fb9cf47356e561b90af0e9df62c1b484a2782a8
aotianlong <aotianlong@hengfirst.com>
2023-05-27 13:47:34 +0800

    去掉管理员tab

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -89,7 +89,7 @@ const show = computed({
           </div>
         </NTabPane>
 
-        <NTabPane name="Account" tab="Account">
+        <NTabPane v-if="false" name="Account" tab="Account">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:bar-chart-box-line" />
             <span class="ml-2">账户信息</span>
@@ -109,42 +109,42 @@ const show = computed({
           </div>
         </NTabPane>
 
-        <NTabPane v-if="userStore.userInfo.root" name="Config" tab="Config">
+        <NTabPane v-if="false && userStore.userInfo.root" name="Config" tab="Config">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:list-settings-line" />
             <span class="ml-2">{{ $t('setting.config') }}</span>
           </template>
           <About />
         </NTabPane>
-        <NTabPane v-if="userStore.userInfo.root" name="SiteConfig" tab="SiteConfig">
+        <NTabPane v-if="false && userStore.userInfo.root" name="SiteConfig" tab="SiteConfig">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:settings-line" />
             <span class="ml-2">{{ $t('setting.siteConfig') }}</span>
           </template>
           <Site />
         </NTabPane>
-        <NTabPane v-if="userStore.userInfo.root" name="MailConfig" tab="MailConfig">
+        <NTabPane v-if="false && userStore.userInfo.root" name="MailConfig" tab="MailConfig">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:mail-line" />
             <span class="ml-2">{{ $t('setting.mailConfig') }}</span>
           </template>
           <Mail />
         </NTabPane>
-        <NTabPane v-if="userStore.userInfo.root" name="AuditConfig" tab="AuditConfig">
+        <NTabPane v-if="false && userStore.userInfo.root" name="AuditConfig" tab="AuditConfig">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:settings-line" />
             <span class="ml-2">{{ $t('setting.auditConfig') }}</span>
           </template>
           <Audit />
         </NTabPane>
-        <NTabPane v-if="userStore.userInfo.root" name="UserConfig" tab="UserConfig">
+        <NTabPane v-if="false && userStore.userInfo.root" name="UserConfig" tab="UserConfig">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri-user-5-line" />
             <span class="ml-2">{{ $t('setting.userConfig') }}</span>
           </template>
           <User />
         </NTabPane>
-        <NTabPane v-if="userStore.userInfo.root" name="KeysConfig" tab="KeysConfig">
+        <NTabPane v-if="false && userStore.userInfo.root" name="KeysConfig" tab="KeysConfig">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri-key-2-line" />
             <span class="ml-2">{{ $t('setting.keysConfig') }}</span>
